62acc179fc34b7c59c3a3a807e9a93e6ca1b8036
Remko Nijzink <remko.nijzink@list.lu>
2019-03-26 10:10:35 +0100

    changes size of plots in timeseries

--- src_py/plot_et_ass.py
+++ src_py/plot_et_ass.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:928b4f1b6e616dacd509dd7dfdebfe4977952282863fa6005edc855831327476
+oid sha256:177f4fd74b097fa654bff4e89bb1b575a65611697d1a74f13ba13a7be3658a65
 size 5234
