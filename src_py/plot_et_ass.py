2997d481fd0c658e63a39c4836585f47e5b8949d
Remko Nijzink <remko.nijzink@list.lu>
2019-07-24 16:52:33 +0200

    added notebook, changed plotscript

--- src_py/plot_et_ass.py
+++ src_py/plot_et_ass.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:177f4fd74b097fa654bff4e89bb1b575a65611697d1a74f13ba13a7be3658a65
-size 5234
+oid sha256:4d4f4184198cd1d33b6b956e0e265d1c0b530963125a0a37e715cdfe4e488376
+size 5503
