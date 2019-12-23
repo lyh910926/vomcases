3dbd9261a15f2282608cb8de9e8129e68199f7ab
Remko Nijzink <remko.nijzink@list.lu>
2019-12-23 16:04:29 +0100

    added fig4c with soil moisture

--- src_py/plot_et_ass.py
+++ src_py/plot_et_ass.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b72497c3eba6f2aa8664aa2a59aeb0a9d1ff6938899301f5e7891fcc55ab91cd
-size 12174
+oid sha256:e4d3877867e52b4502d3ad4f251bb40bf6382490972ab1ae9825190a51c667e4
+size 12189
