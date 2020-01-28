7e022f26e31221757cb0c49bcbb4b37115976269
Remko Nijzink <remko.nijzink@list.lu>
2020-01-28 12:58:41 +0100

    added script to calculate measures of fit models whitley

--- src_py/model_stats.py
+++ src_py/model_stats.py
@@ -13,8 +13,20 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i", "--inputfile", help="results_daily.txt")
-    parser.add_argument("-o", "--outputfolder", help="outputfolder")    
+    parser.add_argument("--bess", help="bess input files")
+    parser.add_argument("--bios2", help="bios2 input files")
+    parser.add_argument("--lpjguess", help="lpj-guess input files, first all files with et, second gpp")
+    parser.add_argument("--maespa", help="maespa input files")
+    parser.add_argument("--spa", help="spa input files")
+    parser.add_argument("--cable", help="cable input files")
+
+    parser.add_argument("--out_bess", help="outputfolder")
+    parser.add_argument("--out_bios2", help="outputfolder")    
+    parser.add_argument("--out_lpjguess", help="outputfolder")    
+    parser.add_argument("--out_maespa", help="outputfolder")    
+    parser.add_argument("--out_spa", help="outputfolder")    
+    parser.add_argument("--out_cable", help="outputfolder")    
+    
     parser.add_argument("-eo", "--evap_obs", help="inputfile with observations evaporation")
     parser.add_argument("-ao", "--ass_obs", help="inputfile with observations assimilation")
     parser.add_argument("-po", "--pc_obs", help="inputfile with observations projective cover")
@@ -26,6 +38,10 @@ def main():
 
     args = parser.parse_args()
 
+    ###################################
+    #some constants for conversions
+    lat_heat_vapor = 2.26   #[MJ/kg]
+    rho_w = 997             #[kg/m3]
        
     ###########################################
     #load observed evaporation
@@ -38,7 +54,7 @@ def main():
             dates_obs = np.append(dates_obs, tmp[0] )
     #make pandas series
     dates_obs = pd.date_range(dates_obs[0],periods=len( dates_obs ), freq='D')
-    eobs_pd = pd.Series(evap_obs, index = dates_obs )
+    eobs_pd = pd.Series(evap_obs, index = dates_obs ) * lat_heat_vapor * rho_w * 1000/(3600*24) 
 
     #load observed assimilation
     ass_obs = np.array([])
@@ -50,81 +66,229 @@ def main():
             dates_obs = np.append(dates_obs, tmp[0] )
     #make pandas series
     dates_obs = pd.date_range(dates_obs[0],periods=len( dates_obs ), freq='D')
+    ass_obs = -1000000*ass_obs/ (3600*24)
     assobs_pd = pd.Series(ass_obs, index = dates_obs )
-
-    #load observed projective cover
-    pcobs = np.genfromtxt(args.pc_obs,delimiter=',', usecols=3, missing_values=-999 )
-    pcobs[pcobs <= 0] = np.nan
-    pcobs = 100*pcobs/0.95 #from fPar to vegetative cover
-    t_pcobs = np.genfromtxt(args.pc_obsdates, dtype='str', delimiter=',')
-    t_pcobs = pd.to_datetime(t_pcobs[:,1], format="%Y%m")
-    pcobs_pd = pd.Series(pcobs, index = t_pcobs )
-
 
 
     #############################
-    #select 10% best runs from sce
+    #read in data from BESS
+    bess, bess_dates = read_bess(args.bess)
+    bess_le = pd.Series(bess[:,0], index = bess_dates)
+    bess_gpp = pd.Series(bess[:,1], index = bess_dates)
 
+    #read in data from BIOS2
+    bios2, bios2_dates = read_bios2(args.bios2)
+    bios2_le = pd.Series(bios2[:,3], index = bios2_dates)
+    bios2_gpp = pd.Series(bios2[:,4], index = bios2_dates)
 
-    tmp = pd.read_csv( args.inputfile, delim_whitespace=True, header = 0)
-    e_tmp = 1000*(np.array(tmp["esoil"]) + np.array(tmp["etmg"]) + np.array(tmp["etmt"]))
-    ass_tmp = (np.array(tmp["assg"]) +  np.array(tmp["asst"] ))
-    pc_tmp = np.array(tmp["pc"]) 
+    #read in data from LPJ-GUESS, ET in W/m2, GPP in umol/m2/s
+    lpjguess, lpjguess2, lpjguess_dates = read_lpjguess(args.lpjguess, args.lpjguess[i+len(whitley_sites)])
+    lpjguess_le = pd.Series(lpjguess, index = lpjguess_dates)
+    lpjguess_gpp = pd.Series(lpjguess2, index = lpjguess_dates)
 
-    #make array of dates
+    #read in data from MAESPA, ET in W m-2, GPP in umol m-2 s-1
+    maespa, maespa_dates = read_maespa(args.maespa)
+    maespa_le = pd.Series(maespa[:,1], index = maespa_dates)
+    maespa_gpp = pd.Series(maespa[:,0], index = maespa_dates)
 
+    #read in data from SPA, ET in W m-2, GPP in mmol m-2 s-1
+    spa, spa_dates = read_spa(args.spa)
+    spa_le = pd.Series(spa[:,1], index = spa_dates)
+    spa_gpp = pd.Series(spa[:,0], index = spa_dates)
 
-    #determine which dates overlap       
-    dates_mod = pd.date_range(str(tmp[tmp.columns[2]][0]) + "/" +
-                              str(tmp[tmp.columns[1]][0]) + "/" +
-                              str(tmp[tmp.columns[0]][0]) ,periods=len( tmp[tmp.columns[0]] ), freq='D')
+    #read in data from CABLE, ET in kg/m^2/s, GPP in umol/m^2/s
+    cable, cable2, cable_dates = read_cable(args.cable)
+    cable_le = pd.Series(cable * lat_heat_vapor * 1000 * 1000 , index = spa_dates)
+    cable_gpp = pd.Series(cable2, index = spa_dates)
 
-    dates_overlap = dates_mod.intersection(eobs_pd.index)
-    print("Evaluating fluxes for:")
+    #############################
+    #BESS statistics
+    dates_overlap = bess_dates.intersection(eobs_pd.index)
+    print("Evaluating BESS fluxes for:")
     print(dates_overlap[0])
     print(dates_overlap[-1])
 
-    dates_overlap_pc = dates_mod.intersection(pcobs_pd.index)
-    print("Evaluating projective cover for:")
-    print(dates_overlap_pc[0])
-    print(dates_overlap_pc[-1])
-
-    #make pandaseries from model results
-    emod_pd = pd.Series(e_tmp, index = dates_mod )
-    assmod_pd = pd.Series(ass_tmp, index = dates_mod )
-    pcmod_pd = pd.Series(pc_tmp, index = dates_mod )
-
     #calc KGE
-    eKGE  = calcKGE(emod_pd[dates_overlap], eobs_pd[dates_overlap])
-    assKGE  = calcKGE(assmod_pd[dates_overlap], assobs_pd[dates_overlap])
-    pcKGE  = calcKGE(pcmod_pd[dates_overlap_pc], pcobs_pd[dates_overlap_pc])
+    eKGE  = calcKGE(bess_le[dates_overlap], eobs_pd[dates_overlap])
+    assKGE  = calcKGE(bess_gpp[dates_overlap], assobs_pd[dates_overlap])
 
-    eMeanAnnRE  = calcREmean(emod_pd[dates_overlap], eobs_pd[dates_overlap])
-    assMeanAnnRE  = calcREmean(assmod_pd[dates_overlap], assobs_pd[dates_overlap])
-    pcMeanAnnRE  = calcREmean(pcmod_pd[dates_overlap_pc], pcobs_pd[dates_overlap_pc])
+    eMeanAnnRE  = calcREmean(bess_le[dates_overlap], eobs_pd[dates_overlap])
+    assMeanAnnRE  = calcREmean(bess_gpp[dates_overlap], assobs_pd[dates_overlap])
 
-    eMAE  = calcMAE(emod_pd[dates_overlap], eobs_pd[dates_overlap])
-    assMAE = calcMAE(assmod_pd[dates_overlap], assobs_pd[dates_overlap])
-    pcMAE  = calcMAE(pcmod_pd[dates_overlap_pc], pcobs_pd[dates_overlap_pc])
+    eMAE  = calcMAE(bess_le[dates_overlap], eobs_pd[dates_overlap])
+    assMAE = calcMAE(bess_gpp[dates_overlap], assobs_pd[dates_overlap])
 
-    eMeanSeas1RE  = calcREmean_seasonal(emod_pd[dates_overlap], eobs_pd[dates_overlap], args.startdry, args.enddry)
-    assMeanSeas1RE  = calcREmean_seasonal(assmod_pd[dates_overlap], assobs_pd[dates_overlap], args.startdry, args.enddry)
-    pcMeanSeas1RE  = calcREmean_seasonal(pcmod_pd[dates_overlap_pc], pcobs_pd[dates_overlap_pc], args.startdry, args.enddry)
+    eMeanSeas1RE  = calcREmean_seasonal(bess_le[dates_overlap], eobs_pd[dates_overlap], args.startdry, args.enddry)
+    assMeanSeas1RE  = calcREmean_seasonal(bess_gpp[dates_overlap], assobs_pd[dates_overlap], args.startdry, args.enddry)
 
-    eMeanSeas2RE  = calcREmean_seasonal(emod_pd[dates_overlap], eobs_pd[dates_overlap], args.startwet, args.endwet)
-    assMeanSeas2RE  = calcREmean_seasonal(assmod_pd[dates_overlap], assobs_pd[dates_overlap], args.startwet, args.endwet)
-    pcMeanSeas2RE  = calcREmean_seasonal(pcmod_pd[dates_overlap_pc], pcobs_pd[dates_overlap_pc], args.startwet, args.endwet)
+    eMeanSeas2RE  = calcREmean_seasonal(bess_le[dates_overlap], eobs_pd[dates_overlap], args.startwet, args.endwet)
+    assMeanSeas2RE  = calcREmean_seasonal(bess_gpp[dates_overlap], assobs_pd[dates_overlap], args.startwet, args.endwet)
 
-
-    #calc residuals
+    #put results together
     eresult = [ eKGE, eMeanAnnRE, eMeanSeas1RE, eMeanSeas2RE, eMAE ]
     assresult = [ assKGE, assMeanAnnRE, assMeanSeas1RE, assMeanSeas2RE, assMAE ]
-    pcresult = [ pcKGE, pcMeanAnnRE, pcMeanSeas1RE, pcMeanSeas2RE, pcMAE ]
  
     #write output files
-    np.savetxt( args.outputfolder + "/evap_beststats.txt", eresult, comments='', delimiter=" " )
-    np.savetxt( args.outputfolder + "/ass_beststats.txt", assresult, comments='', delimiter=" " )
-    np.savetxt( args.outputfolder + "/pc_beststats.txt", pcresult, comments='', delimiter=" " )
+    np.savetxt( args.out_bess + "/evap_beststats.txt", eresult, comments='', delimiter=" " )
+    np.savetxt( args.out_bess + "/ass_beststats.txt", assresult, comments='', delimiter=" " )
+
+    #############################
+    #BIOS2 statistics
+    dates_overlap = BIOS2_dates.intersection(eobs_pd.index)
+    print("Evaluating BIOS2 fluxes for:")
+    print(dates_overlap[0])
+    print(dates_overlap[-1])
+
+    #calc KGE
+    eKGE  = calcKGE(bios2_le[dates_overlap], eobs_pd[dates_overlap])
+    assKGE  = calcKGE(bios2_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanAnnRE  = calcREmean(bios2_le[dates_overlap], eobs_pd[dates_overlap])
+    assMeanAnnRE  = calcREmean(bios2_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMAE  = calcMAE(bios2_le[dates_overlap], eobs_pd[dates_overlap])
+    assMAE = calcMAE(bios2_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanSeas1RE  = calcREmean_seasonal(bios2_le[dates_overlap], eobs_pd[dates_overlap], args.startdry, args.enddry)
+    assMeanSeas1RE  = calcREmean_seasonal(bios2_gpp[dates_overlap], assobs_pd[dates_overlap], args.startdry, args.enddry)
+
+    eMeanSeas2RE  = calcREmean_seasonal(bios2_le[dates_overlap], eobs_pd[dates_overlap], args.startwet, args.endwet)
+    assMeanSeas2RE  = calcREmean_seasonal(bios2_gpp[dates_overlap], assobs_pd[dates_overlap], args.startwet, args.endwet)
+
+    #put results together
+    eresult = [ eKGE, eMeanAnnRE, eMeanSeas1RE, eMeanSeas2RE, eMAE ]
+    assresult = [ assKGE, assMeanAnnRE, assMeanSeas1RE, assMeanSeas2RE, assMAE ]
+ 
+    #write output files
+    np.savetxt( args.out_bios2 + "/evap_beststats.txt", eresult, comments='', delimiter=" " )
+    np.savetxt( args.out_bios2 + "/ass_beststats.txt", assresult, comments='', delimiter=" " )
+
+   #############################
+    #LPJ-GUESS statistics
+    dates_overlap = lpjguess_dates.intersection(eobs_pd.index)
+    print("Evaluating BIOS2 fluxes for:")
+    print(dates_overlap[0])
+    print(dates_overlap[-1])
+
+    #calc KGE
+    eKGE  = calcKGE(lpjguess_le[dates_overlap], eobs_pd[dates_overlap])
+    assKGE  = calcKGE(lpjguess_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanAnnRE  = calcREmean(lpjguess_le[dates_overlap], eobs_pd[dates_overlap])
+    assMeanAnnRE  = calcREmean(lpjguess_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMAE  = calcMAE(lpjguess_le[dates_overlap], eobs_pd[dates_overlap])
+    assMAE = calcMAE(lpjguess_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanSeas1RE  = calcREmean_seasonal(lpjguess_le[dates_overlap], eobs_pd[dates_overlap], args.startdry, args.enddry)
+    assMeanSeas1RE  = calcREmean_seasonal(lpjguess_gpp[dates_overlap], assobs_pd[dates_overlap], args.startdry, args.enddry)
+
+    eMeanSeas2RE  = calcREmean_seasonal(lpjguess_le[dates_overlap], eobs_pd[dates_overlap], args.startwet, args.endwet)
+    assMeanSeas2RE  = calcREmean_seasonal(lpjguess_gpp[dates_overlap], assobs_pd[dates_overlap], args.startwet, args.endwet)
+
+    #put results together
+    eresult = [ eKGE, eMeanAnnRE, eMeanSeas1RE, eMeanSeas2RE, eMAE ]
+    assresult = [ assKGE, assMeanAnnRE, assMeanSeas1RE, assMeanSeas2RE, assMAE ]
+ 
+    #write output files
+    np.savetxt( args.out_lpjguess + "/evap_beststats.txt", eresult, comments='', delimiter=" " )
+    np.savetxt( args.out_lpjguess + "/ass_beststats.txt", assresult, comments='', delimiter=" " )
+
+    #############################
+    #maespa statistics
+    dates_overlap = maespa_dates.intersection(eobs_pd.index)
+    print("Evaluating MAESPA fluxes for:")
+    print(dates_overlap[0])
+    print(dates_overlap[-1])
+
+    #calc KGE
+    eKGE  = calcKGE(maespa_le[dates_overlap], eobs_pd[dates_overlap])
+    assKGE  = calcKGE(maespa_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanAnnRE  = calcREmean(maespa_le[dates_overlap], eobs_pd[dates_overlap])
+    assMeanAnnRE  = calcREmean(maespa_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMAE  = calcMAE(maespa_le[dates_overlap], eobs_pd[dates_overlap])
+    assMAE = calcMAE(maespa_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanSeas1RE  = calcREmean_seasonal(maespa_le[dates_overlap], eobs_pd[dates_overlap], args.startdry, args.enddry)
+    assMeanSeas1RE  = calcREmean_seasonal(maespa_gpp[dates_overlap], assobs_pd[dates_overlap], args.startdry, args.enddry)
+
+    eMeanSeas2RE  = calcREmean_seasonal(maespa_le[dates_overlap], eobs_pd[dates_overlap], args.startwet, args.endwet)
+    assMeanSeas2RE  = calcREmean_seasonal(maespa_gpp[dates_overlap], assobs_pd[dates_overlap], args.startwet, args.endwet)
+
+    #put results together
+    eresult = [ eKGE, eMeanAnnRE, eMeanSeas1RE, eMeanSeas2RE, eMAE ]
+    assresult = [ assKGE, assMeanAnnRE, assMeanSeas1RE, assMeanSeas2RE, assMAE ]
+ 
+    #write output files
+    np.savetxt( args.out_maespa + "/evap_beststats.txt", eresult, comments='', delimiter=" " )
+    np.savetxt( args.out_maespa + "/ass_beststats.txt", assresult, comments='', delimiter=" " )
+
+    #############################
+    #spa statistics
+    dates_overlap = spa_dates.intersection(eobs_pd.index)
+    print("Evaluating SPA fluxes for:")
+    print(dates_overlap[0])
+    print(dates_overlap[-1])
+
+    #calc KGE
+    eKGE  = calcKGE(spa_le[dates_overlap], eobs_pd[dates_overlap])
+    assKGE  = calcKGE(spa_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanAnnRE  = calcREmean(spa_le[dates_overlap], eobs_pd[dates_overlap])
+    assMeanAnnRE  = calcREmean(spa_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMAE  = calcMAE(spa_le[dates_overlap], eobs_pd[dates_overlap])
+    assMAE = calcMAE(spa_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanSeas1RE  = calcREmean_seasonal(spa_le[dates_overlap], eobs_pd[dates_overlap], args.startdry, args.enddry)
+    assMeanSeas1RE  = calcREmean_seasonal(spa_gpp[dates_overlap], assobs_pd[dates_overlap], args.startdry, args.enddry)
+
+    eMeanSeas2RE  = calcREmean_seasonal(spa_le[dates_overlap], eobs_pd[dates_overlap], args.startwet, args.endwet)
+    assMeanSeas2RE  = calcREmean_seasonal(spa_gpp[dates_overlap], assobs_pd[dates_overlap], args.startwet, args.endwet)
+
+    #put results together
+    eresult = [ eKGE, eMeanAnnRE, eMeanSeas1RE, eMeanSeas2RE, eMAE ]
+    assresult = [ assKGE, assMeanAnnRE, assMeanSeas1RE, assMeanSeas2RE, assMAE ]
+ 
+    #write output files
+    np.savetxt( args.out_spa + "/evap_beststats.txt", eresult, comments='', delimiter=" " )
+    np.savetxt( args.out_spa + "/ass_beststats.txt", assresult, comments='', delimiter=" " )
+
+    #############################
+    #cable statistics
+    dates_overlap = cable_dates.intersection(eobs_pd.index)
+    print("Evaluating CABLE fluxes for:")
+    print(dates_overlap[0])
+    print(dates_overlap[-1])
+
+    #calc KGE
+    eKGE  = calcKGE(cable_le[dates_overlap], eobs_pd[dates_overlap])
+    assKGE  = calcKGE(cable_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanAnnRE  = calcREmean(cable_le[dates_overlap], eobs_pd[dates_overlap])
+    assMeanAnnRE  = calcREmean(cable_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMAE  = calcMAE(cable_le[dates_overlap], eobs_pd[dates_overlap])
+    assMAE = calcMAE(cable_gpp[dates_overlap], assobs_pd[dates_overlap])
+
+    eMeanSeas1RE  = calcREmean_seasonal(cable_le[dates_overlap], eobs_pd[dates_overlap], args.startdry, args.enddry)
+    assMeanSeas1RE  = calcREmean_seasonal(cable_gpp[dates_overlap], assobs_pd[dates_overlap], args.startdry, args.enddry)
+
+    eMeanSeas2RE  = calcREmean_seasonal(cable_le[dates_overlap], eobs_pd[dates_overlap], args.startwet, args.endwet)
+    assMeanSeas2RE  = calcREmean_seasonal(cable_gpp[dates_overlap], assobs_pd[dates_overlap], args.startwet, args.endwet)
+
+    #put results together
+    eresult = [ eKGE, eMeanAnnRE, eMeanSeas1RE, eMeanSeas2RE, eMAE ]
+    assresult = [ assKGE, assMeanAnnRE, assMeanSeas1RE, assMeanSeas2RE, assMAE ]
+ 
+    #write output files
+    np.savetxt( args.out_cable + "/evap_beststats.txt", eresult, comments='', delimiter=" " )
+    np.savetxt( args.out_cable + "/ass_beststats.txt", assresult, comments='', delimiter=" " )
+
+
+
 
     print("finished!")
 
@@ -190,6 +354,62 @@ def calcREmean_seasonal(sim, obs, start, end ):
 
         RE = (mu_s-mu_o)/mu_o
         return(RE)
+
+
+def read_bess(infile):
+
+    data = np.loadtxt(infile, delimiter=",") 
+    time = pd.date_range("01-01-2000", periods = len(data[:,0]), freq='D')
+
+    return data, time
+
+def read_bios2(infile):
+
+    data = np.loadtxt(infile, delimiter=",") 
+    time = pd.date_range(datetime(int(data[0,0]),int(data[0,1]),int(data[0,2])), 
+              datetime(int(data[-1,0]),int(data[-1,1]),int(data[-1,2])),freq='D')
+
+    return data, time
+
+def read_lpjguess(infile_et, infile_gpp):
+    data_et = np.loadtxt(infile_et, skiprows=1, usecols=3)
+    data_gpp = np.loadtxt(infile_gpp, skiprows=1, usecols=3)
+    time_tmp = np.loadtxt(infile_gpp, skiprows=1)
+    time = pd.date_range(datetime(int(time_tmp[0,0]),int(time_tmp[0,1]),int(time_tmp[0,2])), 
+              datetime(int(time_tmp[-1,0]),int(time_tmp[-1,1]),int(time_tmp[-1,2])),freq='D')
+
+    return data_et, data_gpp, time
+
+def read_maespa(infile):
+
+    data = np.loadtxt(infile, delimiter=",", skiprows=3, usecols=(3,6))
+    data[data == -9999.9] = np.nan
+    data[data == -999] = np.nan
+    time_tmp = np.loadtxt(infile, delimiter=",", dtype=np.str, skiprows=3, usecols=0)
+    time = pd.date_range(time_tmp[0], time_tmp[-1],freq='30min')
+
+    return data, time
+
+def read_spa(infile):
+    data = np.loadtxt(infile, delimiter=",", skiprows=1, usecols=(1,3))
+    time_tmp = np.loadtxt(infile, delimiter=",", dtype=np.str, skiprows=1, usecols=0)
+    time = pd.date_range(time_tmp[0], time_tmp[-1],freq='30min')
+
+    return data, time
+
+def read_cable(infile):
+    ncfile = Dataset(infile)
+    data_gpp = np.squeeze(ncfile.variables["GPP"]) # extract variable
+    data_et = np.squeeze(ncfile.variables["Evap"]) # extract variable
+    time_tmp = np.squeeze(ncfile.variables["time"]) # extract variable
+    time_tmp = [pd.to_datetime("2007-01-01 00:01:00") + pd.Timedelta(seconds=i) for i in time_tmp]
+    time = pd.date_range(time_tmp[0], time_tmp[-1],freq='30min')
+
+    return data_et, data_gpp, time
+
+
+
+
 
 main()
 
